b9e15edc62cbbd83040b3f00b87f7eb6035c382c
Paul Lavoine <paul.lavoine@teachonmars.com>
2019-03-26 12:58:20 +0100

    Create shared framework

---
+++ IHKeyboardAvoiding/IHKeyboardAvoiding.h
@@ -0,0 +1,19 @@
+//
+//  IHKeyboardAvoiding.h
+//  IHKeyboardAvoiding
+//
+//  Created by Paul Lavoine on 26/03/2019.
+//  Copyright © 2019 IdleHandsApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IHKeyboardAvoiding.
+FOUNDATION_EXPORT double IHKeyboardAvoidingVersionNumber;
+
+//! Project version string for IHKeyboardAvoiding.
+FOUNDATION_EXPORT const unsigned char IHKeyboardAvoidingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IHKeyboardAvoiding/PublicHeader.h>
+
+
